6db8128ea9303299d73ca0f80eaa630c87819a22
aimelive <aimendayambaje24@gmail.com>
2023-06-25 16:01:35 +0200

    version 3

--- src/mailer.ts
+++ src/mailer.ts
@@ -2,7 +2,6 @@ import Mail from "nodemailer/lib/mailer";
 import { MailSend } from "./sender";
 
 export interface MailerConfig {
-  resolverUrl?: string;
   name?: string;
   user: string;
   pass: string;
@@ -13,6 +12,7 @@ export interface MailerOptions extends Omit<Mail.Options, "from" | "replyTo"> {}
 export interface MailerResponse {
   status: "FAIL" | "SUCCESS";
   message: string;
+  error?: any;
 }
 
 export class FlipsideMailer {
@@ -24,21 +24,6 @@ export class FlipsideMailer {
 
   async send(mailOptions: MailerOptions): Promise<MailerResponse> {
     try {
-      // const result = await fetch(
-      //   `${this.config.resolverUrl || "http:localhost:4000"}/api/send`,
-      //   {
-      //     method: "POST",
-      //     headers: {
-      //       Accept: "application/json",
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       auth: this.config,
-      //       mailOptions,
-      //     }),
-      //   }
-      // );
-      // const response = await result.json();
       await MailSend({ auth: this.config, mailOptions });
       return {
         status: "SUCCESS",
@@ -48,6 +33,7 @@ export class FlipsideMailer {
       return {
         status: "FAIL",
         message: error.message || "Email not sent!",
+        error: error,
       };
     }
   }
